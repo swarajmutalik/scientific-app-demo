400ac94b0ef5780deddb89e0f65818fd2ac98de6
swarajmutalik <swarajmutalik@gmail.com>
2026-02-09 15:55:12 +0530

    Made some change

--- test_statistics.py
+++ test_statistics.py
@@ -6,7 +6,7 @@ def test_mean():
     assert mean([2, 4, 6]) == 4
 
 def test_variance():
-    assert variance([2, 4, 6]) == 2.67
+    assert round(variance([2, 4, 6]),2) == 2.67
 
 def test_standard_deviation():
     assert round(standard_deviation([2, 4, 6]), 2) == 1.63
